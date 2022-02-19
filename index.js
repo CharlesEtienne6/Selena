4ad4319276c4fbd3a220eb471e77695b802b820d
CharlesEtienne6 <cisscharlesetienne@gmail.com>
2022-02-19 00:24:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -321,19 +321,6 @@ bot.on("message", message =>{
     }
 })
 
-//!ping
-bot.on("message", async message =>{
-    if(message.content === prefix + "ping"){
-        let msg = await message.channel.send("Ping en cours...")
-
-        let embed = new Discord.MessageEmbed()
-        .addField("Votre ping est de  :", Math.floor(msg.createdAt - message.createdAt))
-        .addField("Ma latence est de ", bot.ws.ping)
-        message.delete()
-        message.channel.send(embed)
-    }
-})
-
 //!avatar
 bot.on('message', message =>{
     if(message.content === prefix + "avatar"){
