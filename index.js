32310b79f2473a803daabb440333112a6fc48846
CharlesEtienne6 <cisscharlesetienne@gmail.com>
2022-02-19 10:35:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ bot.on("message", async message => {
 
     if(message.content.startsWith(prefix)) {
         try{
-            let commandFile = require(`./commands/${command.split(content).slice(prefix.length)}.js`)
+            let commandFile = require(`./commands/${command.split("").slice(prefix.length)}.js`)
             commandFile.execute(bot, message, args);
         } catch (e){
             console.warn(`Erreur avec le handler ${e}`);
